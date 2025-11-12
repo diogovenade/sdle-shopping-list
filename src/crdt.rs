e7842e799026260a2a9db8ea528e366d55b220d9
Ismael Moniz <up202206871@up.pt>
2025-11-12 21:02:13 +0000

    added simple GCounter crdt, tested and working

---
+++ src/crdt.rs
@@ -0,0 +1,145 @@
+use std::cmp;
+use std::collections::HashMap;
+use uuid::Uuid;
+
+#[derive(Clone)]
+pub struct GCounter {
+    counter: HashMap<Uuid, u64>,
+    id: Uuid,
+}
+
+impl GCounter {
+    pub fn new(id: Uuid) -> Self {
+        GCounter {
+            counter: HashMap::new(),
+            id,
+        }
+    }
+
+    pub fn inc(&mut self) {
+        *self.counter.entry(self.id).or_insert(0) += 1;
+    }
+
+    pub fn value_local(&self) -> u64 {
+        *self.counter.get(&self.id).unwrap_or(&0)
+    }
+
+    pub fn value_total(&self) -> u64 {
+        self.counter.values().sum()
+    }
+
+    pub fn merge(&mut self, other: &GCounter) {
+        for (id, count) in &other.counter {
+            let max_count = *cmp::max(count, self.counter.get(id).unwrap_or(&0));
+            self.counter.insert(id.to_owned(), max_count);
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use uuid::Uuid;
+
+    #[test]
+    fn test_local_increment() {
+        let id = Uuid::new_v4();
+        let mut c = GCounter::new(id);
+
+        assert_eq!(c.value_local(), 0);
+        assert_eq!(c.value_total(), 0);
+
+        c.inc();
+        c.inc();
+
+        assert_eq!(c.value_local(), 2);
+        assert_eq!(c.value_total(), 2);
+    }
+
+    #[test]
+    fn test_merge() {
+        let id1 = Uuid::new_v4();
+        let id2 = Uuid::new_v4();
+
+        let mut c1 = GCounter::new(id1);
+        let mut c2 = GCounter::new(id2);
+
+        c1.inc();
+        c1.inc();
+        c2.inc();
+
+        c1.merge(&c2);
+
+        assert_eq!(c1.value_total(), 3);
+        assert_eq!(c1.value_local(), 2);
+        assert_eq!(c2.value_local(), 1);
+    }
+
+    #[test]
+    fn test_merge_commutative() {
+        let id1 = Uuid::new_v4();
+        let id2 = Uuid::new_v4();
+
+        let mut a = GCounter::new(id1);
+        let mut b = GCounter::new(id2);
+
+        a.inc();
+        a.inc();
+        b.inc();
+
+        let mut a_then_b = a.clone();
+        let mut b_then_a = b.clone();
+
+        a_then_b.merge(&b);
+        b_then_a.merge(&a);
+
+        assert_eq!(a_then_b.value_total(), b_then_a.value_total());
+    }
+
+    #[test]
+    fn test_merge_is_idempotent() {
+        let id1 = Uuid::new_v4();
+        let id2 = Uuid::new_v4();
+
+        let mut a = GCounter::new(id1);
+        let mut b = GCounter::new(id2);
+
+        a.inc();
+        b.inc();
+
+        a.merge(&b);
+        let first_merge_value = a.value_total();
+
+        a.merge(&b);
+        let second_merge_value = a.value_total();
+
+        assert_eq!(first_merge_value, second_merge_value);
+    }
+
+    #[test]
+    fn test_merge_is_associative() {
+        let id1 = Uuid::new_v4();
+        let id2 = Uuid::new_v4();
+        let id3 = Uuid::new_v4();
+
+        let mut a = GCounter::new(id1);
+        let mut b = GCounter::new(id2);
+        let mut c = GCounter::new(id3);
+
+        a.inc(); // 1
+        b.inc(); // 1
+        b.inc(); // 2
+        c.inc(); // 1
+
+        let mut ab = a.clone();
+        ab.merge(&b);
+        ab.merge(&c);
+
+        let mut bc = b.clone();
+        bc.merge(&c);
+        let mut a_bc = a.clone();
+        a_bc.merge(&bc);
+
+        assert_eq!(ab.value_total(), a_bc.value_total());
+    }
+}
