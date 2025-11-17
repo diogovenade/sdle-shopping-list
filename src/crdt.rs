b7baad7222fef5521bc37d8c72757839178fa14e
Ismael Moniz <up202206871@up.pt>
2025-11-17 15:02:15 +0000

    added PNCounter crdt

--- src/crdt.rs
+++ src/crdt.rs
@@ -2,6 +2,51 @@ use std::cmp;
 use std::collections::HashMap;
 use uuid::Uuid;
 
+// Item
+pub struct Item {
+    id: Uuid,
+    name: String,
+    amount: u32,
+    acquired: bool,
+}
+
+// PNCounter
+pub struct PNCounter {
+    p: GCounter,
+    n: GCounter,
+}
+
+impl PNCounter {
+    pub fn new(id: Uuid) -> Self {
+        PNCounter {
+            p: GCounter::new(id),
+            n: GCounter::new(id),
+        }
+    }
+
+    pub fn inc(&mut self) {
+        self.p.inc();
+    }
+
+    pub fn dec(&mut self) {
+        self.n.inc();
+    }
+
+    pub fn value_local(&self) -> u64 {
+        self.p.value_local() - self.n.value_local()
+    }
+
+    pub fn value_total(&self) -> u64 {
+        self.p.value_total() - self.n.value_total()
+    }
+
+    pub fn merge(&mut self, other: &PNCounter) {
+        self.p.merge(&other.p);
+        self.n.merge(&other.n);
+    }
+}
+
+// GCounter
 #[derive(Clone)]
 pub struct GCounter {
     counter: HashMap<Uuid, u64>,
@@ -41,8 +86,9 @@ mod tests {
     use super::*;
     use uuid::Uuid;
 
+    // GCounter
     #[test]
-    fn test_local_increment() {
+    fn test_gcounter_local_increment() {
         let id = Uuid::new_v4();
         let mut c = GCounter::new(id);
 
@@ -57,7 +103,7 @@ mod tests {
     }
 
     #[test]
-    fn test_merge() {
+    fn test_gcounter_merge() {
         let id1 = Uuid::new_v4();
         let id2 = Uuid::new_v4();
 
@@ -76,7 +122,7 @@ mod tests {
     }
 
     #[test]
-    fn test_merge_commutative() {
+    fn test_gcounter_merge_commutative() {
         let id1 = Uuid::new_v4();
         let id2 = Uuid::new_v4();
 
@@ -97,7 +143,7 @@ mod tests {
     }
 
     #[test]
-    fn test_merge_is_idempotent() {
+    fn test_gcounter_merge_is_idempotent() {
         let id1 = Uuid::new_v4();
         let id2 = Uuid::new_v4();
 
@@ -117,7 +163,7 @@ mod tests {
     }
 
     #[test]
-    fn test_merge_is_associative() {
+    fn test_gcounter_merge_is_associative() {
         let id1 = Uuid::new_v4();
         let id2 = Uuid::new_v4();
         let id3 = Uuid::new_v4();
@@ -141,5 +187,91 @@ mod tests {
         a_bc.merge(&bc);
 
         assert_eq!(ab.value_total(), a_bc.value_total());
+    }
+
+    // PNCounter
+    #[test]
+    fn test_pncounter_local_value() {
+        let id = Uuid::new_v4();
+        let mut counter = PNCounter::new(id);
+
+        assert_eq!(counter.value_local(), 0);
+
+        counter.inc();
+        assert_eq!(counter.value_local(), 1);
+
+        counter.dec();
+        assert_eq!(counter.value_local(), 0);
+
+        counter.inc();
+        counter.inc();
+        counter.dec();
+        assert_eq!(counter.value_local(), 1);
+    }
+
+    #[test]
+    fn test_pncounter_merge_counters() {
+        let id1 = Uuid::new_v4();
+        let id2 = Uuid::new_v4();
+
+        let mut c1 = PNCounter::new(id1);
+        let mut c2 = PNCounter::new(id2);
+
+        c1.inc();
+        c1.inc();
+        c2.inc();
+        c2.dec();
+
+        assert_eq!(c1.value_total(), 2);
+        assert_eq!(c2.value_total(), 0);
+
+        c1.merge(&c2);
+        c2.merge(&c1);
+
+        assert_eq!(c1.value_total(), 2);
+        assert_eq!(c2.value_total(), 2);
+    }
+
+    #[test]
+    fn test_pncounter_convergence() {
+        let id1 = Uuid::new_v4();
+        let id2 = Uuid::new_v4();
+
+        let mut c1 = PNCounter::new(id1);
+        let mut c2 = PNCounter::new(id2);
+
+        for _ in 0..5 {
+            c1.inc();
+        }
+        for _ in 0..3 {
+            c2.inc();
+        }
+        for _ in 0..2 {
+            c1.dec();
+        }
+        for _ in 0..1 {
+            c2.dec();
+        }
+
+        c1.merge(&c2);
+        c2.merge(&c1);
+
+        assert_eq!(c1.value_total(), 5 + 3 - 2 - 1);
+        assert_eq!(c2.value_total(), 5);
+    }
+
+    #[test]
+    fn test_local_values_are_correct() {
+        let id = Uuid::new_v4();
+        let mut c = PNCounter::new(id);
+
+        for _ in 0..4 {
+            c.inc();
+        }
+        for _ in 0..2 {
+            c.dec();
+        }
+
+        assert_eq!(c.value_local(), 2);
     }
 }
